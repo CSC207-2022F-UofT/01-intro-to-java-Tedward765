945729ace4879fc0fc5ccd4f4d35621eb607d8ba
Tedward765 <teddyleung123@gmail.com>
2022-09-20 17:37:59 -0400

    cool

--- src/main/java/Basics.java
+++ src/main/java/Basics.java
@@ -7,6 +7,8 @@
  * relevant readings for each task.
  */
 
+import java.lang.reflect.Array;
+
 /**
  *  1. In Java, no code can exist outside of a class. Unlike Python which uses
  *    functions, all code in Java uses methods.
@@ -39,10 +41,7 @@ public class Basics {
          *    (Relevant reading: 1.1.3. Printing things)
          */
         System.out.println(7 + 5);
-
-        /* TODO (Task 1): Write a line of code below that prints the string
-         *                Hello World!
-         */
+        System.out.println("Hello World!");
 
 
 
@@ -57,11 +56,7 @@ public class Basics {
          *
          *    (Relevant reading: 1.2. Variables and Types)
          */
-
-        /*
-         * TODO (Task 2): Create a variable named my_variable and assign it the
-         *                value 100.
-         */
+        int my_variable = 100;
 
 
 
@@ -86,11 +81,10 @@ public class Basics {
          *
          *    (Relevant reading: 1.8.2. for Loops)
          */
-
+        for (int i = 10; i >= 0; i--){
+            System.out.println("Current count: " + i);
+        }
         /*
-         * TODO (Task 3): Create a for-loop that loops from 10 to 0, printing
-         *                the following at each iteration:
-         *                Current count: #
          *
          * e.g. the end result should look like:
          * Current count: 10
@@ -99,7 +93,6 @@ public class Basics {
          * Current count: 1
          * Current count: 0
          */
-
 
     }
 
@@ -133,16 +126,14 @@ public class Basics {
      * @return           The first letter of every word in to_split
      */
     public static String split(String to_split) {
-        /* TODO (Task 4): Complete this method body.
-         *                The String methods .split and .charAt may be helpful,
-         *                along with the StringBuilder.append
-         *                You may also assume that to_split always has 7 words
-         *                separated by spaces, so no loops are required, though
-         *                you may use them if you wish.
-         */
+
         StringBuilder ret = new StringBuilder();
 
-        // Fill in the rest of the body here
+        String[] words = to_split.split(" ");
+
+        for (String word: words) {
+            ret.append(word.charAt(0));
+        }
 
         return ret.toString();
     }
@@ -164,24 +155,14 @@ public class Basics {
     public static int oddSum(int[] arr) {
         int current_sum = 0;
 
-        /* TODO (Task 5): Complete this method body using a for-loop.
-         *                You can find the length of an array by using the
-         *                .length attribute (e.g. arr.length)
-         *                You can index into arrays as we do in Python
-         *                (e.g. arr[i] gives you the item at index i).
-         */
+        for (int i = 0; i < arr.length; i++){
+            if (i % 2 == 1){
+                current_sum += arr[i];
+            }
+        }
 
         return current_sum;
     }
 
 
-    /*
-     * TODO (Task 6): Submit the changes you made on GitHub!
-     *                When you submit it, go to the 'Actions' tab. You should
-     *                be able to see this exercise being autograded!
-     *
-     *                You can click on the ongoing action to see more details.
-     *                If the tests don't pass, look at the results and revise
-     *                accordingly.
-     */
 }
